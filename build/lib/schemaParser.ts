3f0e8100f097cce91ec8fac0be754567b2fe4afb
Jesper Engberg <jeengbe@gmail.com>
2021-11-25 09:23:36 +0100

    More parser work

--- build/lib/schemaParser.ts
+++ build/lib/schemaParser.ts
@@ -3,7 +3,6 @@ import * as fs from "fs";
 import * as path from "path";
 import * as tts from "ttypescript";
 import * as ts from "typescript";
-import { parseModule } from "./graphQLParser";
 import type { Module } from "./manager";
 import { getInstalledModules } from "./manager";
 
@@ -42,7 +41,7 @@ export type ValueType = {
   type: "void";
 });
 
-function getObjectName(node: ts.ClassDeclaration): string | undefined {
+function getObjectName(node: tsType): string | undefined {
   return node.name?.text;
 }
 
@@ -50,35 +49,100 @@ function getClassByName(src: ts.SourceFile, name: string): ts.ClassDeclaration |
   return src.statements.find(node => node.kind === ts.SyntaxKind.ClassDeclaration && getObjectName(node as ts.ClassDeclaration) === name) as ts.ClassDeclaration | undefined;
 }
 
+function getObjectMethods(object: tsType): ts.MethodDeclaration[] | ts.MethodSignature[] {
+  if (ts.isClassDeclaration(object)) {
+    return object.members.filter(member => ts.isMethodDeclaration(member)) as ts.MethodDeclaration[];
+  } else if (ts.isInterfaceDeclaration(object)) {
+    return object.members.filter(member => member.kind === ts.SyntaxKind.MethodSignature) as ts.MethodSignature[];
+  }
+  return [];
+}
+
+function getObjectProperties(object: tsType): ts.PropertyDeclaration[] | ts.PropertySignature[] {
+
+}
+
+type tsType = ts.ClassDeclaration | ts.InterfaceDeclaration | ts.TypeAliasDeclaration | ts.EnumDeclaration;
+
+const types: Record<string, Type> = {};
+const parsing: string[] = [];
+
+function parseType(src: ts.SourceFile, object: tsType): void {
+  const name = getObjectName(object);
+  if (!name) {
+    throw new Error("Unnamed object");
+  }
+  if (name in types || parsing.includes(name)) {
+    // We have already parsed this type or are currently parsing it (circular dependency)
+    return;
+  }
+
+  parsing.push(name);
+
+  const methods = getObjectMethods(object);
+  const properties = getObjectProperties(object, true);
+
+  const methodsAndPropertiesOrderedByLineNumber = methods.concat(properties).sort((a, b) => a.getStart() - b.getStart());
+
+  const type: Type = {
+    name,
+    description: getObjectDescription(object),
+    fields: []
+  };
+
+  for (const methodOrProperty of methodsAndPropertiesOrderedByLineNumber) {
+    if (ts.isMethodDeclaration(methodOrProperty)) {
+      resolveMethodDepencies(methodOrProperty);
+      type.fields.push({
+        name: getMethodName(methodOrProperty),
+        description: getMethodDescription(methodOrProperty),
+        type: getMethodType(methodOrProperty),
+        args: getMethodArgs(methodOrProperty)
+      });
+    } else {
+      resolveFieldDepencies(methodOrProperty);
+      type.fields.push({
+        name: getFieldName(methodOrProperty),
+        description: getFieldDescription(methodOrProperty),
+        type: getFieldType(methodOrProperty),
+        args: []
+      });
+    }
+  }
+
+  Object.assign(types[name], type);
+}
+
+function parseModule(module: Module): void {
+  const entryFilePath = path.resolve(__modules.backend, module.id, "schema.ts");
+
+  const src = tts.createSourceFile(entryFilePath, fs.readFileSync(entryFilePath, "utf-8"), tts.ScriptTarget.ES2020);
+  const queryClass = getClassByName(src, "Query");
+
+  parseClass(src, queryClass);
+
+  const objects: Record<string, any> = {};
+  const objectsToCheck = [queryClass];
+
+  while (objectsToCheck.length > 0) {
+    const objectToCheck = objectsToCheck.pop()!;
+    // We can be sure that all objects have a name
+    const objectName = getObjectName(objectToCheck)!;
+    if (objectName in objects) continue; // We already got this
+    // Get dependencies
+    const dependencies = getDependencies(objectToCheck);
+    const dependenciesInFiles = getFilesOfDependencies(dependencies, src);
+    objectsToCheck.push(...getObjectsInDependencies(dependenciesInFiles));
+  }
+}
+
+
 export class SchemaParser {
   protected readonly types: Record<string, Type> = {};
 
   public addModule(...modules: Module[]): void {
     for (const module of modules) {
       parseModule(module);
-    }
-  }
-
-  public parseModule(module: Module): void {
-    const entryFilePath = path.resolve(__modules.backend, module.id, "schema.ts");
-    // get depending classes, interfaces and types
-    // recursively parse those and their dependencies
-
-    const src = tts.createSourceFile(entryFilePath, fs.readFileSync(entryFilePath, "utf-8"), tts.ScriptTarget.ES2020);
-    const queryClass = getClassByName(src, "Query");
-
-    const objects: Record<string, any> = {};
-    const objectsToCheck = [queryClass];
-
-    while (objectsToCheck.length > 0) {
-      const objectToCheck = objectsToCheck.pop()!;
-      // We can be sure that all objects have a name
-      const objectName = getObjectName(objectToCheck)!;
-      if (objectName in objects) continue; // We already got this
-      // Get dependencies
-      const dependencies = getDependencies(objectToCheck);
-      const dependenciesInFiles = getFilesOfDependencies(dependencies, src);
-      objectsToCheck.push(...getObjectsInDependencies(dependenciesInFiles));
     }
   }
 }
