6ead1876ff05ba2f6691ab7027a9f5c7b0b775a5
Jesper <jeengbe@gmail.com>
2021-12-31 13:25:14 +0100

    Some Schwekas progress

--- src/frontend/modules/@Schwekas/management/components/projects/overview/ProjectsOverview.tsx
+++ src/frontend/modules/@Schwekas/management/components/projects/overview/ProjectsOverview.tsx
@@ -1,21 +1,65 @@
 import { Component } from "@core/components/Component";
 import classNames from "classnames";
 import * as React from "react";
+import { ProjectsOverviewDisplayMode } from "../../../pages/Projects";
 import { settings } from "../../../pages/Projects";
 import css from "./Layout.scss";
 
 interface Props {
-  variant: typeof settings.projects.overview.displayMode;
+  variant: ProjectsOverviewDisplayMode;
+  // TODO: Structure data
+  data: any;
 }
 
 export class ProjectsOverview extends Component<Props> {
   public render(): JSX.Element {
     const { variant, data } = this.props;
 
-    return (
-      <div className={classNames(className, css.container)} {...props}>
-        {children}
-      </div>
-    );
+    switch (variant) {
+      case ProjectsOverviewDisplayMode.TABLE:
+        return (
+          <Table
+            striped
+            columns={[
+              {
+                title: "Name",
+                field: ({ name, domain }) => (
+                  <>
+                    <b>{name}</b>
+                    <br />
+                    {domain}
+                  </>
+                ),
+              },
+              {
+                title: "Beschreibung",
+                field: "description",
+              },
+              {
+                title: "Status",
+                field: ({ status }) =>
+                  ({
+                    good: <Badge>Gut</Badge>,
+                    warning: <Badge>Warnung</Badge>,
+                  }[status]),
+              },
+            ]}
+            request={filter => gql`
+              query() {
+                projects(${filter}) {
+                  id
+                  name
+                  description
+                  status
+                }
+              }
+            `}
+            // Replace with loading someday
+            data={data}
+          />
+        );
+      case ProjectsOverviewDisplayMode.CARDS:
+        return data.map(project => <Card />);
+    }
   }
 }
--- src/frontend/modules/@Schwekas/management/pages/Projects.tsx
+++ src/frontend/modules/@Schwekas/management/pages/Projects.tsx
@@ -1,15 +1,37 @@
 import { Page } from "@core/Page";
 import * as React from "react";
 import { Route } from "@core/routeDecorator";
+import { ProjectsOverview } from "../components/projects/overview/ProjectsOverview";
+
+export enum ProjectsOverviewDisplayMode {
+  "TABLE",
+  "CARDS",
+}
 
 // TODO: Settings as a whole
 export declare const settings: {
   projects: {
     overview: {
-      displayMode: "TABLE" | "CARDS";
+      displayMode: ProjectsOverviewDisplayMode;
     };
   };
 };
+
+// TODO: Load data
+const data = [
+  {
+    name: "KTool",
+    domain: "ktool.schwekas.de",
+    description: "Managementsoftware für Autohäuser",
+    status: "good",
+  },
+  {
+    name: "Novia",
+    domain: "novia.schwekas.de",
+    description: "Verwaltung für SKJ",
+    status: "good",
+  },
+];
 
 @Route("/Projekte")
 export class Projects extends Page {
@@ -21,26 +43,13 @@ export class Projects extends Page {
     } = settings;
 
     return (
-      <Shell>
+      <>
         <Title>Projekte</Title>
-        <Heading size="3">Projektübersicht</Heading>
-        <Loader
-          // TODO: Problem for another day
-          request={gql`
-            query {
-              projects(${filter}) {
-                id,
-                name,
-                description,
-                coverImage,
-                status
-              }
-            }
-        `}
-        >
-          {data => <ProjectsOverview variant={displayMode} data={data} />}
-        </Loader>
-      </Shell>
+        <Shell>
+          <Heading size="3">Projektübersicht</Heading>
+          <ProjectsOverview displayMode={displayMode} data={data} />
+        </Shell>
+      </>
     );
   }
 }
